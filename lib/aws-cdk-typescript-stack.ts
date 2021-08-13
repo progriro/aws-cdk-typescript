4b9370b358ea631d89e1f109fcc2298f38733c6a
progriro <progriro@gmail.com>
2021-08-13 17:57:34 +0900

    modified lib/aws-cdk-typescript-stack.ts for ec2 and vpc

--- lib/aws-cdk-typescript-stack.ts
+++ lib/aws-cdk-typescript-stack.ts
@@ -1,9 +1,19 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
 export class AwsCdkTypescriptStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'TheVPC', {
+      cidr: "10.0.0.0/16"
+    })
+
+    const instance = new ec2.Instance(this, id, {
+      instanceType: new ec2.InstanceType("t2.micro"),
+      machineImage: new ec2.AmazonLinuxImage,
+      vpc: vpc
+    })
   }
 }
